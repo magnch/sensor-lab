9439eeb32d6b415e4706d1625bd02a8e2c7ba745
magnch <magnus.11387@gmail.com>
2025-03-12 21:40:58 +0100

    Commented

--- lab3.py
+++ lab3.py
@@ -5,10 +5,12 @@ filebase = "transmittans_måling_"
 rgb_peaks = np.zeros((5, 3))
 filter = True
 
+# Loop over all files, plot RGB values and FFT for each 
 for i in range(5):
     filename = filebase + str(i+1) + ".csv"
     r, g, b = import_rgb(filename)
 
+    # Apply bandpass filter
     if filter:
         r = bandpass_filter(r)
         g = bandpass_filter(g)
@@ -20,8 +22,10 @@ for i in range(5):
     plot_rgb_fft(freqs, r_f, g_f, b_f, filename, f_min=0.5, f_max=2.5)
     print(f"FFT for {filename} plotted.")
     print("---------------------------------------------------")
+    # Extract peak values for each color
     rgb_peaks[i] = extract_peak_rgb(freqs, r_f, g_f, b_f, f_min=0.5, f_max=2.5)
 
+# Loop over all robusthet files, plot RGB values and FFT for each
 for maling in ["lys", "hoy_puls"]:
     filename = "robusthet_" + maling + ".csv" 
     r, g, b = import_rgb(filename)
@@ -38,7 +42,7 @@ for maling in ["lys", "hoy_puls"]:
     print(f"FFT for {filename} plotted.")
     print("---------------------------------------------------")
 
-
+# Calculate mean and std for each color
 for i, color in enumerate(["R", "G", "B"]):
     # Calculate mean and std for each color
     mean, std = calculate_mean_and_std(rgb_peaks[:, i])
